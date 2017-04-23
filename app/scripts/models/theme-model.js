4d1c162da0a64aaded3a9fe0a3943a5ffea76b01
Enrico Monese <enricomonese@gmail.com>
2017-04-23 15:11:21 +0200

    Add theme model

---
+++ app/scripts/models/theme-model.js
@@ -0,0 +1,38 @@
+const Backbone = require('backbone');
+const SettingsStore = require('../comp/settings-store');
+
+const ThemeModel = Backbone.Model.extend({
+    defaults: {
+        colorMain: '#282C34',
+        mediumColor: '#ABB2BF',
+        textColor: '#D7DAE0',
+        actionColor: '#528BFF',
+        errorColor: '#C34034',
+
+        mutePercent: 30,
+        lightBorderPercent: 10,
+        modalOpacity: .9,
+        colorLightnessShift: 0
+    },
+
+    initialize: function() {
+        this.listenTo(this, 'change', this.save);
+    },
+
+    load: function() {
+        return SettingsStore.load('theming').then(data => {
+            if (data) {
+                this.upgrade(data);
+                this.set(data, {silent: true});
+            }
+        });
+    },
+
+    save: function() {
+        SettingsStore.save('theming', this.attributes);
+    }
+});
+
+// ThemeModel.instance = new ThemeModel();
+
+module.exports = ThemeModel;
